175c7be3bf9ada5ebc02bce97a3da2e77abdd500
Konrad Sroga <konradsroga@gmail.com>
2025-02-12 00:42:27 +0100

    [task-129] Update

--- src/Http/Controllers/SSOAuthorizationController.php
+++ src/Http/Controllers/SSOAuthorizationController.php
@@ -2,17 +2,13 @@
 
 namespace Pteroca\PterodactylAddon\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use Pteroca\PterodactylAddon\Http\Requests\SSOLoginRequest;
 use Pteroca\PterodactylAddon\Services\JwtService;
 use Pterodactyl\Http\Controllers\Api\Application\ApplicationApiController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use Carbon\CarbonImmutable;
 use Pterodactyl\Models\User;
 use Illuminate\Auth\AuthManager;
 use Illuminate\Container\Container;
-use Illuminate\Support\Facades\Event;
 use Pterodactyl\Events\Auth\DirectLogin;
 use Pterodactyl\Http\Controllers\Auth\AbstractLoginController;
 
@@ -23,6 +19,7 @@ class SSOAuthorizationController extends AbstractLoginController
     public function __construct(JwtService $jwtService)
     {
         parent::__construct();
+
         $this->jwtService = $jwtService;
     }
 
@@ -45,20 +42,7 @@ class SSOAuthorizationController extends AbstractLoginController
             return response()->json(['error' => 'Cannot login with 2FA enabled'], 403);
         }
 
-        Auth::login($user);
-
-        $request->session()->put('auth_confirmation_token', [
-            'user_id' => $user->id,
-            'token_value' => $token = Str::random(64),
-            'expires_at' => CarbonImmutable::now()->addMinutes(5),
-        ]);
-
-        //Event::dispatch(new DirectLogin($user, Container::getInstance()->make(AuthManager::class)));
-        $request->session()->regenerate();
-
-        return $this->sendLoginResponse($user, $request);
-
-        dd(Auth::check(), Auth::user());
+        $this->sendLoginResponse($user, $request);
 
         return redirect()->intended('/');
     }
